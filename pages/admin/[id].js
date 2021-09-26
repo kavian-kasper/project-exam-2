ec6d812519010a9549b987520eae1bc78654706b
kavian-kasper <60392158+kavian-kasper@users.noreply.github.com>
2021-09-27 00:34:19 +0200

    added revalidate to two remaining getStaticProps

--- pages/admin/[id].js
+++ pages/admin/[id].js
@@ -31,6 +31,7 @@ export async function getStaticPaths() {
   return {
       props: {
         data },
+        revalidate: 30
     }
   }
 
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -26,6 +26,7 @@ export async function getStaticPaths() {
 
     return {
       props: { data },
+      revalidate: 30
     }
   }
 
